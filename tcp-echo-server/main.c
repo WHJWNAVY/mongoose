fc2a01e2c39954eef594016feb30ea97bced4728
WHJWNAVY <whjwnavy@163.com>
2022-06-07 13:23:44 +0800

    code optimization

--- tcp-echo-server/main.c
+++ tcp-echo-server/main.c
@@ -1,17 +1,23 @@
 #include "mongoose.h"
 
-static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
-  if (ev == MG_EV_READ) {
-    mg_send(c, c->recv.buf, c->recv.len);     // Echo received data back
-    mg_iobuf_del(&c->recv, 0, c->recv.len);   // And discard it
-  }
+static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data)
+{
+    if (ev == MG_EV_READ)
+    {
+        MG_INFO(("Recved [%s]\n", c->recv.buf));
+        mg_send(c, c->recv.buf, c->recv.len);   // Echo received data back
+        mg_iobuf_del(&c->recv, 0, c->recv.len); // And discard it
+    }
 }
 
-int main(int argc, char *argv[]) {
-  struct mg_mgr mgr;
-  mg_mgr_init(&mgr);                                // Init manager
-  mg_listen(&mgr, "tcp://0.0.0.0:1234", cb, &mgr);  // Setup listener
-  for (;;) mg_mgr_poll(&mgr, 1000);                 // Event loop
-  mg_mgr_free(&mgr);                                // Cleanup
-  return 0;
+int main(int argc, char *argv[])
+{
+    struct mg_mgr mgr;
+    // mg_log_set("4");
+    mg_mgr_init(&mgr);                               // Init manager
+    mg_listen(&mgr, "tcp://0.0.0.0:1234", cb, &mgr); // Setup listener
+    for (;;)
+        mg_mgr_poll(&mgr, 1000); // Event loop
+    mg_mgr_free(&mgr);           // Cleanup
+    return 0;
 }
